9613882a6a544df5f0fb7970f9e64f99466d605d
Agnar Martin Bjørnstad <agnar.bjornstad@gmail.com>
2025-05-02 13:36:00 +0200

    fix: update predict.sh to use ahus_predict.py

--- src/submission/predict.sh
+++ src/submission/predict.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-python3 sammed_predict.py --load_path "/workspace/inputs" --save_path "/workspace/outputs" --checkpoint "/workspace/weights.pth"
+python3 ahus_predict.py --load_path "/workspace/inputs" --save_path "/workspace/outputs" --model_checkpoint "/workspace/weights.pth" --model_type "ahus_model_rope_mixed" --segmenter_type "original"
