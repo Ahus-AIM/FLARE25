9042dc81c1ff8b5e69049c3adfacce81dca46553
Valter Schutz <valterschutz@proton.me>
2025-05-09 20:26:00 +0200

    refactor: tensordict dataset accepts separate gt dir

--- src/dataset/multiclass_npz_dataset.py
+++ src/dataset/multiclass_npz_dataset.py
@@ -13,34 +13,34 @@ def dicts_to_tensor_boxes(box_dict: np.ndarray) -> torch.Tensor:
     """
     boxes = torch.zeros((len(box_dict), 2, 3), dtype=torch.int32)
     for i, box in enumerate(box_dict):
-        boxes[i, 0, :] = torch.tensor(
-            [box["z_min"], box["z_mid_y_min"], box["z_mid_x_min"]], dtype=torch.int32
-        )
-        boxes[i, 1, :] = torch.tensor(
-            [box["z_max"], box["z_mid_y_max"], box["z_mid_x_max"]], dtype=torch.int32
-        )
+        boxes[i, 0, :] = torch.tensor([box["z_min"], box["z_mid_y_min"], box["z_mid_x_min"]], dtype=torch.int32)
+        boxes[i, 1, :] = torch.tensor([box["z_max"], box["z_mid_y_max"], box["z_mid_x_max"]], dtype=torch.int32)
     return boxes
 
 
-def get_tensordict_iterator(data_dir: Path) -> Iterator[TensorDict]:
-    """Return an iterator over the files in the data_dir, yielding TensorDicts with no batch size, with the keys
+# TODO: potential improvement: crop to bounding boxes
+
+
+def get_tensordict_iterator(val_dir: Path, val_gt_dir: Path) -> Iterator[TensorDict]:
+    """Iterate over files in val_dir, yielding TensorDicts with no batch size, with the keys
     - "image": the image as a float32 tensor of shape (D, H, W)
     - "boxes": the boxes as an int32 tensor of shape (n_instances, 2, 3)
-    - "spacings": the spacing for each instance, as a float32 tensor of shape (n_instances, 3)
-    - "true_multiclass_segmentation": the true multiclass segmentation as a uint8 tensor of shape (D, H, W)
+    - "spacings": the spacing, as a float32 tensor of shape (3,)
+    - "true_multiclass_segmentation": the true multiclass segmentation as a uint8 tensor of shape (D, H, W). Read from val_gt_dir.
+
+    Note: Since the original ground truth is binary and batched, we assume that no instances are overlapping when we take the argmax.
     """
-    file_paths: list[Path] = list(data_dir.iterdir())
+    file_paths: list[Path] = list(val_dir.iterdir())
 
     for file_path in file_paths:
-        data = np.load(file_path, allow_pickle=True)
+        val_data = np.load(file_path, allow_pickle=True)
+        val_gt_data = np.load(val_gt_dir / file_path.name, allow_pickle=True)
         yield TensorDict(
             {
-                "image": torch.tensor(data["imgs"] / 255.0, dtype=torch.float32),
-                "boxes": dicts_to_tensor_boxes(data["boxes"]),
-                "spacings": torch.tensor(data["spacing"], dtype=torch.float32),
-                "true_multiclass_segmentation": torch.tensor(
-                    data["gts"], dtype=torch.uint8
-                ),
+                "image": torch.tensor(val_data["imgs"] / 255.0, dtype=torch.float32),
+                "boxes": dicts_to_tensor_boxes(val_data["boxes"]),
+                "spacing": torch.tensor(val_data["spacing"], dtype=torch.float32),
+                "true_multiclass_segmentation": torch.tensor(val_gt_data["gts"], dtype=torch.uint8),
             },
             batch_size=(),
         )
